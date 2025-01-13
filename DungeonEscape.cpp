526b0491054fd33d6cea62db9f52157ad54e11bd
aleksanii3 <aleksandar.ivanov005@gmail.com>
2025-01-13 09:26:00 +0200

    Add files via upload
    Движение на играча

--- DungeonEscape.cpp
+++ DungeonEscape.cpp
@@ -6,7 +6,13 @@ using namespace std;
 const int MAX_MAP_ROWS = 10;
 const int MAX_MAP_COLS = 15;
 
-bool load_map (const char *filepath, char map[]) {
+struct Game {
+	char map[MAX_MAP_ROWS * MAX_MAP_COLS];
+	int player_x;
+	int player_y;
+};
+
+bool load_map (Game *game, const char *filepath) {
 	ifstream file(filepath);
 	if (!file.is_open()) {
 		return false;
@@ -14,7 +20,12 @@ bool load_map (const char *filepath, char map[]) {
 
 	for (int row = 0; row < MAX_MAP_ROWS; row++) {
 		for (int col = 0; col < MAX_MAP_COLS; col++) {
-			map[row * MAX_MAP_COLS + col] = file.get();
+			char c = file.get();
+			game->map[row * MAX_MAP_COLS + col] = c;
+			if (c == '@') {
+				game->player_x = col;
+				game->player_y = row;
+			}
 		}
 		if (file.get() != '\n') {
 			file.close();
@@ -26,21 +37,53 @@ bool load_map (const char *filepath, char map[]) {
 	return true;
 }
 
-void print_map(const char *map) {
+void print_map(Game *game) {
 	for (int row = 0; row < MAX_MAP_ROWS; row++) {
 		for (int col = 0; col < MAX_MAP_COLS; col++) {
-			cout << map[row * MAX_MAP_COLS + col];
+			if (game->player_x == col && game->player_y == row) {
+				cout << '@';
+			}
+			else {
+				char c = game->map[row * MAX_MAP_COLS + col];
+				if (c == '@') {
+					cout << ' ';
+				}
+				else {
+					cout << c;
+				}
+			}
 		}
 		cout << endl;
 	}
 }
 
+void move_player(Game *game) {
+	char input;
+	cin >> input;
+
+	if (input == 'a' && game->player_x > 0) {
+		game->player_x--;
+	}
+	else if (input == 'd' && game->player_x < MAX_MAP_COLS - 1) {
+		game->player_x++;
+	}
+	else if (input == 'w' && game->player_y > 0) {
+		game->player_y--;
+	}
+	else if (input == 's' && game->player_y < MAX_MAP_ROWS - 1) {
+		game->player_y++;
+	}
+}
+
 int main () {
-	char map[MAX_MAP_ROWS * MAX_MAP_COLS];
-	if (!load_map("map1.txt", map))
+	Game game = {};
+	if (!load_map(&game, "map1.txt"))
 	  return -1;
 
-	print_map(map);
+	while (true) {
+		print_map(&game);
+		move_player(&game);
+	}
 
 	return 0;
 }
